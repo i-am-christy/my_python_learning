2319e2bfc69762dffe9bdecd71de2489841e5fd6
Christianah <kunlechristy2019@gmail.com>
2022-10-01 12:19:14 +0100

    python number script

---
+++ python_numbers.py
@@ -0,0 +1,29 @@
+#integers
+num1 = 100
+print(num1)
+num2 = -10
+print(num2)
+num3 = 1_000_000
+print(num3)
+print(type(num1), type(num2), type(num2))
+num4 = int(5.0345)
+print(num4)
+num5 = int("65")
+print(num5)
+num6 = int("100", 2)
+print(num6)
+num7 = int("3234", 8)
+print(num7)
+#binary
+num8 = 0b111101110101
+print(num8, type(num8))
+num9 = 0b1_001_111_101
+print(num9)
+#octal
+num10 = 0o12
+print(num10, type(num10))
+num11 = 0o12345667
+print(num11, type(num11))
+#hexadecimal
+num12 = 0x123Afde
+print(num12, type(num12))
